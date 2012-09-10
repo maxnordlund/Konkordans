2f61344beb44bc7eb79b25d98982fc5ad2f2fec7
Max Nordlund <max.nordlund@gmail.com>
2012-09-10 19:04:33 +0200

    This actually works

--- indexer.py
+++ indexer.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
 import sys
 import os
 
-KORPUS = "/info/adk12/labb1/korpus"
+KORPUS     = "/info/adk12/labb1/korpus"
+STRIPPER   = "\"\n\t!%&'()*+,-./0123456789:;=?_€§¤©®·°[]$<>@´`"
 KONKORDANS = "index.txt"
 
 def create_konkordans(filename):
-    words = set()
+    words = {}
     
-    with open(KORPUS, encoding="ISO-8859-1") as korpus:
-        for line in korpus:
-            for word in line.split():
-                words.add(word.strip("!\"%&'()*+,-./0123456789:;=?_€§©®·[]$<>@`").lower())
-        
-        print("No. unique words: ", len(words))
-        with open(filename, encoding="UTF-8", mode="wt") as index:
-            for word in sorted(words):
-                
-                index.write(word + "\n")
+    with open(KORPUS, encoding="ISO-8859-1") as f:
+        i = f.tell()
+        for line in f:
+            for word in line.split(" "):
+                i += 1
+                stripped = word.strip(STRIPPER).lower()
+                if stripped != "":
+                    if stripped not in words:
+                        words[stripped] = [i]
+                    else:
+                        words[stripped].append(i)
+                    #l = words.setdefault(stripped, list())
+                    #l.append(i)
+                    i += len(word)
+            i += len(line)
+    
+    words = OrderedDict(sorted(words.items(), key=lambda t: t[0]))
+
+    with open(filename, encoding="UTF-8", mode="wt") as f:
+        for t in words.items():
+            #f.write(t[0] + " " + str(t[1]) + "\n")
+            f.write(str(len(t[1])) + " " + t[0] + "\n")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -28,3 +42,4 @@ if __name__ == "__main__":
         # Behöver vi skapa konkordansen?
         if not os.path.isfile(KONKORDANS):
             create_konkordans(KONKORDANS)
+
