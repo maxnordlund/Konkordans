b43d0c04674291094466a6ee488bd12f0325a616
strokirk <dan.strokirk@gmail.com>
2012-09-13 23:54:57 +0200

    Lots of changes, main functionality should be implemented by now. Going home soon\!

--- Index.py
+++ Index.py
@@ -1,59 +1,97 @@
 #!/usr/bin/env python3
 
+import struct
 import Links.Links as Links
+import Hash.Hash as Hash
+
+ENCODING = "ISO-8859-1" # Every character is 1 byte
 
 class Index:
     """En klass som pratar med indexfilen"""
     def __init__(self, korpus, index_path="index.dat", link_path="links.dat"):
+        self.chunk_size  = 0
         self._korpus     = korpus
         self._index_path = index_path
         self._link_path  = link_path
-        with open("hash.dat", mode="r+b") as h:
-            self._hash   = Hash(h)
+        self._hash       = None
+        self._links      = None
     
     def __enter__(self):
-        self._fil   = open(self._index_path, mode="r+b", encoding=ISO).__enter__()
-        self._links = open(self._link_path,  mode="r+b")
+        self._links_file = open(self._link_path,  mode="r+b")
+        self._fil = open(self._index_path, mode="r+b", encoding=ENCODING))
+        self._fil.__enter__()
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         return self._fil.__exit__(exc_type, exc_val, exc_tb)
     
-    def create(self):
+    def build(self):
+        # Reading all words from our korpus
         index   = self._korpus.tell()
         words   = {}
-        max_len = 0
+        longest_word = 0
         for line in self._korpus:
             for word in line.split(" "):
                 index += 1
                 stripped = word.strip(STRIPPER).lower()
-                if stripped != "":
-                    if stripped not in words:
-                        words[stripped] = [index]
-                    else:
-                        words[stripped].append(i)
-                    index += len(word)
+
+                if stripped == "": # Don't add strange empty words
+                    continue
+
+                if stripped not in words:
+                    words[stripped] = [index]
+                else:
+                    words[stripped].append(i)
+                index += len(word)
+                longest_word = max(longest_word, len(word))
             index += len(line)
         
-        self._links = Links()
-        
-        with open(filename, encoding="UTF-8", mode="wt") as f:
-            for word in sorted(words):'
-                pass
-                #f.write(str(lazy_hash(k,3)) + " " + str(k[4:]))
-                #f.write(word + "\n")
-                #f.write(str(len(t[1])) + " " + t[0] + "\n")
+        # Building our index files
+
+        # Build the word Links index file
+        self._links = Links(self._links_file)
+        range_mapping = self._links.build(words) 
+        # from Links: (word, offset, size)
+
+        # Build the main Index file
+        word_indices = [] # To be added to the Hash index
+
+        format_string = str(longest_word) + "sII"
+        self.chunk_size = struct.calcsize(format_string)
+        for word, rstart, rstop in range_mapping:
+            word_indices.append(word, f.tell())
+            
+            # pack the data as bytes
+            wordbytes = bytes(word.rjust(longest_word), encoding=ENCODING)
+            chunk = struct.pack(format_string, wordbytes, rstart, rstop)
+
+            self._fil.write(chunk)
+
+        # Build the Hash index file
+        self._hash = Hash("hash.dat", word_indices) 
     
     def __getitem__(self, key):
+        if not self._hash:
+            self._hash = Hash("hash.dat") 
+
         if key is not str:
             raise TypeError
         low, high = self._hash[key]
+        format_string = str(longest_word) + "s"
         while low < high:
             mid = (high - low)/2
-            if self._index[mid] < key:
+            mid = mid - mid % self.chunk_size
+            self._fil.seek(mid, 0)
+            data = self._fil.read(self.chunk_size)
+            values = struct.unpack(format_string, data)
+            if values[0] < key:
                 low  = mid
             else:
                 high = mid
-        return self._links[low]
+
+        self._fil.seek(low, 0)
+        data = self._fil.read(self.chunk_size)
+        values = struct.unpack(format_string, data)
+        return self._links.get(values[1], values[2])
 
 
--- Links.py
+++ Links.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
+import struct 
+
 class Links
-    """En klass som pratar med filen med alla ords positioner"""
-    def __init__(self, index):
-        self._index = index
+    """En klass som pratar med filen med alla ords offseter"""
+    def __init__(self, f):
+        self._file = f
+
+    def build(self, words):
+        range_mapping = []
+
+        self._file.seek(0)
+        for word in sorted(words):
+            indices = words[word]
+            format_string = str(len(indices)+1) + "I"
+
+            start = self._file.tell()
+            chunk_size = struct.calcsize(format_string)
+            range_mapping.append( (word, start, chunk_size) )
+
+            chunk = struct.pack(format_string, *indices)
+            self._file.write(chunk)
+
+        return range_mapping
+
+    def get(self, offset, size):
+        self._file.seek(offset, 0) # find the index position
+        data = self._file.read(size)
+        format_string = "I"*(size / struct.calcsize("I") )
+        return struct.unpack(format_string, data)
+
